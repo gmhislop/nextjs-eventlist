900bfaeca52583735200016939fa9e9a3a3f1e61
“gmhislop” <g.hislop@live.nl>
2023-03-13 14:04:10 +0100

    init project

---
+++ pages/events/[eventId].js
@@ -0,0 +1,6 @@
+const EventDetailPage = () => {
+  return (
+    <div><h1>Event Detail</h1></div>
+  )
+}
+export default EventDetailPage;
---
+++ pages/events/index.js
@@ -0,0 +1,6 @@
+const AllEventsPage = () => {
+  return (
+    <div><h1>All Events Page</h1></div>
+  )
+}
+export default AllEventsPage;
