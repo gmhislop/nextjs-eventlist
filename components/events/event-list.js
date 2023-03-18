e1aaf6fc0b00ba7513091828cfbfec5622b4be66
“gmhislop” <g.hislop@live.nl>
2023-03-18 21:04:50 +0100

    fix: added api data

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,12 +1,18 @@
 import EventItem from './event-item';
 import styles from './event-list.module.css';
+import useSWR from 'swr'
 
 const EventList = (props) => {
-  const { items } = props;
+  // const { items } = props;
+  const fetcher = (url) => fetch(url).then(res => res.json());
+  const { data, error, isLoading } = useSWR('https://nextjs-course-8cb56-default-rtdb.firebaseio.com/events.json', fetcher)
+  
+    if (error) return <div>Failed to fetch users.</div>
+    if (isLoading) return <h2>Loading...</h2>
   
   return (
     <ul className={styles.list}>
-      {items.map((event) => (
+      {data.map((event) => (
         <EventItem 
         key={event.id}
         id={event.id} 
