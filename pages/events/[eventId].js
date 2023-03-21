5ce67239bb918a7964ec901026cf9c1b5af559ef
“gmhislop” <g.hislop@live.nl>
2023-03-21 22:28:38 +0100

    fix: typo in firebase

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,53 +1,59 @@
 import { Fragment } from 'react';
+
+import { getEventById, getFeaturedEvents } from '../../helpers/api-util';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
-import { getEventById, getFeaturedEvents } from '../../helpers/api-util';
 
-const EventDetailPage = (props) => {
-
+function EventDetailPage(props) {
   const event = props.selectedEvent;
 
   if (!event) {
-    return <div className='center'><p>Loading...</p></div>;
+    return (
+      <div className="center">
+        <p>Loading...</p>
+      </div>
+    );
   }
 
   return (
     <Fragment>
       <EventSummary title={event.title} />
-      <EventLogistics 
-      date={event.date}
-      address={event.location} 
-      image={event.image} 
-      alt={event.title} 
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
       />
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
     </Fragment>
   );
-};
+}
 
-export const getStaticProps = async (context) => {
+export async function getStaticProps(context) {
   const eventId = context.params.eventId;
 
- const event = await getEventById(eventId);
-
- return {
-  props: {
-    selectedEvent: event,
-  }
- }
-};
-export const getStaticPaths = async () => {
-  const events = await getFeaturedEvents();
-
-  const paths = events.map((event) => ({ params: { eventId: event.id }}))
+  const event = await getEventById(eventId);
 
   return {
-    path: paths,
-    fallback: 'blocking',
-  }
-  
+    props: {
+      selectedEvent: event
+    },
+    revalidate: 30
+  };
 }
+
+export async function getStaticPaths() {
+  const events = await getFeaturedEvents();
+
+  const paths = events.map(event => ({ params: { eventId: event.id } }));
+
+  return {
+    paths: paths,
+    fallback: 'blocking'
+  };
+}
+
 export default EventDetailPage;
